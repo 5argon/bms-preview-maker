1281b3133c4a132ee597f0c5a0b6b0f4af95e8f4
5argon <pyasry@gmail.com>
2017-12-25 15:09:21 +0700

    Fix wrong argument interfacing with bms-renderer

--- worker.js
+++ worker.js
@@ -2,7 +2,7 @@ const bmsRenderer = require('bms-renderer')
 
 onmessage = function(e) {
     console.log("Start working!")
-    bmsRenderer.render(e.data[0], e.data[1], e.data[2], e.data[3]).then( (buffer) =>
+    bmsRenderer.render(e.data[0], e.data[2], e.data[3]).then( (buffer) =>
     {
         console.log("Stop working!")
         postMessage([buffer, e.data[1]])
